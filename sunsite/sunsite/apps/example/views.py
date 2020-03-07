3d69edc958c4345387318f0f68b79036be825173
Naruto-sys <gavriil.belov@yandex.ru>
2020-03-07 20:31:56 +0300

    Added temperature's scraper

--- sunsite/sunsite/apps/example/views.py
+++ sunsite/sunsite/apps/example/views.py
@@ -16,30 +16,35 @@ def index(request):
         for city in response.json()['data']:
             city_id = city['city_id']
             city_name = city['city_name']
-            link = 'https://dt.miet.ru/ppo_it/api/' + str(city_id) + '/'
+            link = LINK + str(city_id) + '/'
             response_city = requests.get(link, headers=HEADER)
+
             if not check_response(response_city):
                 return HttpResponse("Error: " + str(response_city))
-            for i in range(1, response_city.json()["data"]["area_count"] + 1):
-                link = 'https://dt.miet.ru/ppo_it/api/' + str(city_id) + '/' + str(i) + '/'
+
+            for i in range(1, 5):
+                link = LINK + str(city_id) + '/' + str(i) + '/'
                 response_district = requests.get(link, headers=HEADER)
-                flats = 0
+
                 if not check_response(response_district):
                     return HttpResponse("Error: " + str(response_district))
-                for house in response_district.json()['data']:
-                    if flats > 3:
-                        break
+
+                for j in range(3):
+                    house = response_district.json()['data'][j]
                     link1 = link + str(house["house_id"]) + '/'
                     response_house = requests.get(link1, headers=HEADER)
+
                     if not check_response(response_house):
                         return HttpResponse("Error: " + str(response_house))
-                    for j in range(1, response_house.json()['data']['apartment_count']):
-                        link2 = link1 + str(j) + '/'
+
+                    for k in range(1, 4):
+                        link2 = link1 + str(k) + '/temperature/'
                         response_flat = requests.get(link2, headers=HEADER)
+
                         if not check_response(response_city):
                             return HttpResponse("Error: " + str(response_city))
-                        temperature = response_flat.json()['data']['temperature']
-                        flats += 1
+
+                        temperature = response_flat.json()['data']
 
     except Exception as e:
         return HttpResponse("Error: " + e.__class__.__name__)
