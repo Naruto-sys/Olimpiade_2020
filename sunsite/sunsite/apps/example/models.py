67ca6c558afa7c60d17c92df088574657af21739
Naruto-sys <gavriil.belov@yandex.ru>
2020-03-07 21:35:10 +0300

    Updated program

--- sunsite/sunsite/apps/example/models.py
+++ sunsite/sunsite/apps/example/models.py
@@ -2,8 +2,8 @@ from django.db import models
 
 
 class Area(models.Model):
-    town = models.IntegerField('city', default=0)
-    district = models.IntegerField('area', default=0)
+    city = models.IntegerField('city', default=0)
+    area = models.IntegerField('area', default=0)
     house = models.IntegerField('house', default=0)
     flat = models.IntegerField('flat', default=0)
-    temperature = models.TextField('indications', max_length=1300)
+    indications = models.TextField('indications', max_length=1300)
--- sunsite/sunsite/apps/example/views.py
+++ sunsite/sunsite/apps/example/views.py
@@ -22,29 +22,31 @@ def index(request):
             if not check_response(response_city):
                 return HttpResponse("Error: " + str(response_city))
 
-            for i in range(1, 5):
-                link = LINK + str(city_id) + '/' + str(i) + '/'
+            for area in range(1, 5):
+                link = LINK + str(city_id) + '/' + str(area) + '/'
                 response_district = requests.get(link, headers=HEADER)
 
                 if not check_response(response_district):
                     return HttpResponse("Error: " + str(response_district))
 
-                for j in range(3):
-                    house = response_district.json()['data'][j]
+                for home in range(3):
+                    house = response_district.json()['data'][home]
                     link1 = link + str(house["house_id"]) + '/'
                     response_house = requests.get(link1, headers=HEADER)
 
                     if not check_response(response_house):
                         return HttpResponse("Error: " + str(response_house))
 
-                    for k in range(1, 4):
-                        link2 = link1 + str(k) + '/temperature/'
+                    for flat in range(1, 4):
+                        link2 = link1 + str(flat) + '/temperature/'
                         response_flat = requests.get(link2, headers=HEADER)
 
                         if not check_response(response_city):
                             return HttpResponse("Error: " + str(response_city))
 
                         temperature = response_flat.json()['data']
+                        # if Area.objects.get(city=city_id and area=area)
+
 
     except Exception as e:
         return HttpResponse("Error: " + e.__class__.__name__)
