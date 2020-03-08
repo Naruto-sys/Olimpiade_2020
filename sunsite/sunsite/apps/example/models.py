2f07941b39bbe4efad04af530c31df3908521907
Naruto-sys <gavriil.belov@yandex.ru>
2020-03-08 19:51:52 +0300

    Fix adding in databases

--- sunsite/sunsite/apps/example/models.py
+++ sunsite/sunsite/apps/example/models.py
@@ -2,8 +2,15 @@ from django.db import models
 
 
 class Area(models.Model):
+    id = models.IntegerField(primary_key=True)
     city = models.IntegerField('city', default=0)
     area = models.IntegerField('area', default=0)
     house = models.IntegerField('house', default=0)
     flat = models.IntegerField('flat', default=0)
+    indications = models.TextField('indications', max_length=1300)
+
+
+class City(models.Model):
+    id = models.IntegerField(primary_key=True)
+    city = models.IntegerField('city', default=0)
     indications = models.TextField('indications', max_length=1300)
--- sunsite/sunsite/apps/example/views.py
+++ sunsite/sunsite/apps/example/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from .models import Area
+from .models import Area, City
 import requests
 from .Functions import check_response
+import datetime
+import time
 
 
 def index(request):
@@ -12,53 +14,66 @@ def index(request):
     response = requests.get(LINK, headers=HEADER)
     if not check_response(response):
         return HttpResponse("Error: " + str(response))
-    try:
-        for city in response.json()['data']:
-            city_id = city['city_id']
-            city_name = city['city_name']
-            link = LINK + str(city_id) + '/'
-            response_city = requests.get(link, headers=HEADER)
 
-            if not check_response(response_city):
-                return HttpResponse("Error: " + str(response_city))
+    first_time = datetime.datetime.now()
+    while (datetime.datetime.now() - first_time).days < 1:
+        time_iter = datetime.datetime.now()
+        try:
+            for city in response.json()['data']:
+                city_id = city['city_id']
+                city_name = city['city_name']
+                link = LINK + str(city_id) + '/'
+                response_city = requests.get(link, headers=HEADER)
+                temperature = str(response_city.json()["data"]["temperature"])
+                try:
+                    obj = City.objects.get(city=city_id)
+                    # '*' - разделение температур
+                    data = obj.indications
+                    obj.indications = data + '*' + temperature
+                    obj.save()
+                except Exception:
+                    obj = City(city=city_id, indications=temperature)
+                    obj.save()
 
-            for area in range(1, 5):
-                link = LINK + str(city_id) + '/' + str(area) + '/'
-                response_district = requests.get(link, headers=HEADER)
+                if not check_response(response_city):
+                    continue
 
-                if not check_response(response_district):
-                    return HttpResponse("Error: " + str(response_district))
+                for area in range(1, 5):
+                    link = LINK + str(city_id) + '/' + str(area) + '/'
+                    response_district = requests.get(link, headers=HEADER)
 
-                for home in range(3):
-                    house = response_district.json()['data'][home]
-                    link1 = link + str(house["house_id"]) + '/'
-                    response_house = requests.get(link1, headers=HEADER)
+                    if not check_response(response_district):
+                        continue
 
-                    if not check_response(response_house):
-                        return HttpResponse("Error: " + str(response_house))
+                    for home in range(1, 4):
+                        house = response_district.json()['data'][home]
+                        link1 = link + str(house["house_id"]) + '/'
+                        response_house = requests.get(link1, headers=HEADER)
 
-                    for flat in range(1, 4):
-                        link2 = link1 + str(flat) + '/temperature/'
-                        response_flat = requests.get(link2, headers=HEADER)
+                        if not check_response(response_house):
+                            continue
 
-                        if not check_response(response_city):
-                            return HttpResponse("Error: " + str(response_city))
+                        for flat in range(1, 4):
+                            link2 = link1 + str(flat) + '/temperature/'
+                            response_flat = requests.get(link2, headers=HEADER)
 
-                        temperature = str(response_flat.json()['data'])
+                            if not check_response(response_city):
+                                continue
 
-                        try:
-                            obj = Area.objects.get(city=city_id, area=area, house=home, flat=flat)
-                            # вот тут поправить
-                            # '*' - разделение температур
-                            data = obj.indications
-                            obj.indications = data + '*' + temperature
-                            obj.save()
-                        except Exception as a:
-                            obj = Area(city=city_id, area=area, house=home, flat=flat, indications=temperature)
-                            obj.save()
+                            temperature = str(response_flat.json()['data'])
 
-    except Exception as e:
-        return HttpResponse("Error: " + e.__class__.__name__)
+                            try:
+                                obj = Area.objects.get(city=city_id, area=area, house=home, flat=flat)
+                                # '*' - разделение температур
+                                data = obj.indications
+                                obj.indications = data + '*' + temperature
+                                obj.save()
+                            except Exception:
+                                obj = Area(city=city_id, area=area, house=home, flat=flat, indications=temperature)
+                                obj.save()
+            if (datetime.datetime.now() - time_iter).seconds < 320:
+                time.sleep(320 - (datetime.datetime.now() - time_iter).seconds)
 
-    return HttpResponse("Privet! Dasha! Rabotai!")
-
+        except Exception as e:
+            continue
+    return HttpResponse("Privet! Solnishko! Rabotai!")
