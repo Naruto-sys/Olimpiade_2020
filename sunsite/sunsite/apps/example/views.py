79ea9b86e21e965402ac56943554f2cee9a05257
Naruto-sys <gavriil.belov@yandex.ru>
2020-03-10 18:42:01 +0300

    Added first question page

--- sunsite/sunsite/apps/example/views.py
+++ sunsite/sunsite/apps/example/views.py
@@ -84,15 +84,53 @@ def main(request):
     LINK = 'https://dt.miet.ru/ppo_it/api/'
     HEADER = {'X-Auth-Token': 'qohuw6zrr4z74qn2'}
     response = requests.get(LINK, headers=HEADER)
-    cities = [_['city_name'] for _ in response.json()['data']]
 
     submitbutton = request.POST.get("submit")
-    choosen_city = request.POST.get("select_city")
-    print(choosen_city)
+    city = request.POST.get("city")
+    area = request.POST.get("area")
+    house = request.POST.get("house")
+    flat = request.POST.get("flat")
+
+    temperature = ''
+    city_number = ''
+
+    if area is None:
+        area = ''
+    if house is None:
+        house = ''
+    if flat is None:
+        flat = ''
+
+    cities = [_['city_name'] for _ in response.json()['data']]
+
+    if city is not None:
+        c = [cities[cities.index(city)]]
+        [c.append(_) for _ in cities[:cities.index(city)]]
+        [c.append(_) for _ in cities[cities.index(city) + 1:]]
+        cities = c
+    if submitbutton is not None:
+        for town in response.json()['data']:
+            if town['city_name'] == city:
+                city_number = str(town['city_id'])
+                break
+
+        final_link = LINK + city_number + '/' + area + '/' + house + '/' + flat + '/'
+        final_response = requests.get(final_link, headers=HEADER)
+        try:
+            temperature = int(final_response.json()['data']['temperature'])
+            temperature = str(final_response.json()['data']['temperature']) + '°'
+            print(temperature)
+        except Exception:
+            temperature = 'Адрес введён некорректно!'
+
     context = {
-        'choosen_city': choosen_city,
+        'temperature': temperature,
+        'area': area,
+        'house': house,
+        'flat': flat,
+        'city': city,
         'cities': cities,
         'submitbutton': submitbutton,
     }
-
+    print(area, house, flat, city)
     return render(request, 'example/page.html', context)
